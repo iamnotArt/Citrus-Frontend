bc15be25e8618ae37d20ac6cce5b21152403ac57
Art <lyndonehemplo1@gmail.com>
2025-05-20 18:48:06 +0800

    signup commit

--- app/src/main/java/com/example/citrusapp/onboardingScreen/OnboardingScreen.kt
+++ app/src/main/java/com/example/citrusapp/onboardingScreen/OnboardingScreen.kt
@@ -1,7 +1,6 @@
 package com.example.citrusapp.onboardingScreen
 
 import AutoScrollPager
-import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.indication
--- app/src/main/java/com/example/citrusapp/signup/Slide3.kt
+++ app/src/main/java/com/example/citrusapp/signup/Slide3.kt
@@ -1,22 +1,69 @@
 package com.example.citrusapp.signup.slides
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import com.example.citrusapp.ui.theme.blue_green
 
 @Composable
 fun SlideThree() {
-    Column(
+    Box(
         modifier = Modifier
             .fillMaxSize()
-            .padding(horizontal = 32.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
     ) {
-        Text("Slide 3")
-        Spacer(modifier = Modifier.height(8.dp))
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(bottom = 50.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(150.dp)
+                    .padding(horizontal = 16.dp)
+                    .background(
+                        color = Color(0xFFE0F7FA),
+                        shape = RoundedCornerShape(24.dp)
+                    )
+                    .padding(16.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                Text("This is a rounded box", color = Color.Black)
+            }
+        }
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.BottomCenter)
+                .padding(bottom = 112.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Button(
+                onClick = {
+                    // TODO: Back to login
+                },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = blue_green,
+                    contentColor = Color.White
+                ),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(48.dp)
+                    .padding(start = 12.dp, end = 12.dp)
+            ) {
+                Text(text = "Finish")
+            }
+        }
     }
 }
